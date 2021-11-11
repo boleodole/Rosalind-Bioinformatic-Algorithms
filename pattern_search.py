1bc6ba01ed0abad2685c6526d502765907e5347e
Bojan Balic <67595772+boleodole@users.noreply.github.com>
2021-11-11 14:35:19 +0100

    Update pattern_search.py

--- pattern_search.py
+++ pattern_search.py
@@ -1,4 +1,3 @@
-# Copy your PatternCount function from the previous step below this line
 def PatternCount(Text, Pattern):
     count = 0
     for i in range(len(Text)-len(Pattern)+1):
@@ -6,7 +5,6 @@ def PatternCount(Text, Pattern):
             count = count+1
     return count
 
-# Now, set Text equal to the ori of Vibrio cholerae and Pattern equal to "TGATCA"
 
 Text = "CGCGATACGTTACATACATGATAGACCGCGCGCGATCATATCGCGATTATC"
 
@@ -14,7 +12,3 @@ Pattern = "CGCG"
 
 count = PatternCount(Text, Pattern)
 print(count)
-
-
-# Finally, print the result of calling PatternCount on Text and Pattern.
-# Don't forget to use the notation print() with parentheses included!
